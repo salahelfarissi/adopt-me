45b7e5a98c3fc121d4dc2a70094e12b6d385344e
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-30 14:06:28 +0200

    Use Pet in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import Pet from './Pet';
 
 const App = () => {
-  return createElement('div', {}, [
-    createElement('h1', {}, 'Adopt me!'),
-    createElement(Pet, {
-      animal: 'Dog',
-      name: 'Luna',
-      breed: 'Havanese',
-    }),
-    createElement(Pet, {
-      animal: 'Bird',
-      name: 'Pepper',
-      breed: 'Cockateil',
-    }),
-    createElement(Pet, {
-      animal: 'Cat',
-      name: 'Doink',
-      breed: 'Mix',
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockateil" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(createElement(App));
+root.render(<App />);
