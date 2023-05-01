81bedabe9965be903f9540231e931899622c8ba5
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-05-01 13:39:54 +0200

    Add handleIndexClick

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -9,6 +9,16 @@ class Carousel extends Component {
     images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
   };
 
+  handleIndexClick = (e) => {
+    this.setState({
+      // The + in front of e.target.dataset.index is to convert the string to a number
+      // It's called a unary operator
+      // There are other ways to convert a string to a number
+      // Like parseInt() or Number()
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -17,7 +27,10 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
+              data-index={index}
               key={photo}
               src={photo}
               className={index === active ? 'active' : ''}
