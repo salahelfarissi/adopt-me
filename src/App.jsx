672d81004a6ef8f2264750998c258eb8758d6b17
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-05-01 13:20:44 +0200

    Add Carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import SearchParams from './components/SearchParams/SearchParams';
-import Details from './components/Details/Details';
+import SearchParams from './components/SearchParams';
+import Details from './components/Details';
 
 const queryClient = new QueryClient({
   defaultOptions: {
--- src/components/SearchParams/index.jsx
+++ src/components/SearchParams/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import useBreedList from './useBreedList';
 import fetchSearch from './fetchSearch';
-import Results from '../Results/Results';
+import Results from '../Results';
 
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 
