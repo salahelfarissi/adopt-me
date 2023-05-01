ef2cccf8ff62f45a826cee40057fc6d8dd71ced3
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-05-01 10:46:03 +0200

    Couple fetchBreedList with useQuery

---
+++ src/fetchBreedList.js
@@ -0,0 +1,18 @@
+const fetchBreedList = async ({ queryKey }) => {
+  const animal = queryKey[1];
+
+  if (!animal) return [];
+
+  const apiRes = await fetch(
+    `https://pets-v2.dev-apis.com/breeds?animal=${animal}`,
+  );
+
+  if (!apiRes.ok) {
+    throw new Error(`breeds/${animal} failed with status ${apiRes.status}`);
+  }
+
+  // Do not need to use await here because we are returning the promise
+  return apiRes.json();
+};
+
+export default fetchBreedList;
