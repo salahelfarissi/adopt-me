3c2e1d8b9f0704c65d7cd461e73f81ebfa2f50e7
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-05-05 16:02:17 +0200

    Use lazy suspense to code split

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { StrictMode, useState } from 'react';
+import { StrictMode, useState, lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import SearchParams from './components/SearchParams';
-import Details from './components/Details';
+const SearchParams = lazy(() => import('./components/SearchParams'));
+const Details = lazy(() => import('./components/Details'));
+
 import AdoptedPetContext from './AdoptedPetContext';
 
 const queryClient = new QueryClient({
@@ -21,15 +22,23 @@ const App = () => {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />}></Route>
-            <Route path="/" element={<SearchParams />}></Route>
-          </Routes>
-        </AdoptedPetContext.Provider>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">🐶</h2>
+            </div>
+          }
+        >
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header>
+              <Link to="/">Adopt me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />}></Route>
+              <Route path="/" element={<SearchParams />}></Route>
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </Suspense>
       </QueryClientProvider>
     </BrowserRouter>
   );
