8d90d644d556024cda2fdd148141dd1904a17ce5
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-05-17 14:39:36 +0200

    Add API response types

---
+++ src/APIResponsesTypes.ts
@@ -0,0 +1,20 @@
+export type Animal = 'cat' | 'dog' | 'bird' | 'rabbit' | 'reptile';
+
+export interface Pet {
+  id: number;
+  name: string;
+  animal: Animal;
+  breed: string;
+  images: string[];
+  description: string;
+  city: string;
+  state: string;
+}
+
+export interface PetAPIResponse {
+  numberOfResults: number;
+  startIndex: number;
+  endIndex: number;
+  hasNext: boolean;
+  pets: Pet[];
+}
