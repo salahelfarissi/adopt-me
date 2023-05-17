af85bb2c79a8d934f80a16b58e39b75f6d626b9d
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-05-17 10:11:46 +0200

    Add suspense true to QueryClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useState, lazy, Suspense } from 'react';
+import { useState, lazy, Suspense } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -12,6 +12,7 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
+      suspense: true,
     },
   },
 });
