35058f53ddffd4ac082ecaec33887276aff0bf7e
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-05-18 21:19:11 +0200

    Create store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState, lazy, Suspense } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import store from './store';
+import { createRoot } from 'react-dom/client';
 
 const SearchParams = lazy(() => import('./components/SearchParams'));
 const Details = lazy(() => import('./components/Details'));
-
-import AdoptedPetContext from './AdoptedPetContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,7 +18,6 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
-  const adoptedPet = useState(null);
   return (
     <QueryClientProvider client={queryClient}>
       <Suspense
@@ -27,18 +27,22 @@ const App = () => {
           </div>
         }
       >
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />}></Route>
-            <Route path="/" element={<SearchParams />}></Route>
-          </Routes>
-        </AdoptedPetContext.Provider>
+        <BrowserRouter>
+          <Provider store={store}>
+            <header>
+              <Link to="/">Adopt me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />}></Route>
+              <Route path="/" element={<SearchParams />}></Route>
+            </Routes>
+          </Provider>
+        </BrowserRouter>
       </Suspense>
     </QueryClientProvider>
   );
 };
 
-export default App;
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
