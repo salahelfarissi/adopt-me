8b1c79b77cda603bd093edb39b67a5012341bafb
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-05-18 12:47:18 +0200

    Add comment to AdoptedPetContext

--- src/AdoptedPetContext.ts
+++ src/AdoptedPetContext.ts
@@ -1,6 +1,9 @@
 import { createContext } from 'react';
 import { Pet } from './components/Details/APIResponsesTypes';
 
+// This is a context that will be used to store the pet that the user has
+// adopted. It is initialized with a dummy value so that TypeScript does not
+// complain about the value being null.
 const AdoptedPetContext = createContext<
   [Pet | null, (adoptedPet: Pet) => void]
 >([
