1f3da6aa90939109b2f44233785154d57a82d56a
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-05-20 15:24:28 +0200

    Test using mocks

--- src/__tests__/useBreedList.test.jsx
+++ src/__tests__/useBreedList.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
 import store from "../store";
@@ -29,4 +29,28 @@ test("returns an empty array when no animal is passed", async () => {
   const [breedList, status] = result.current;
   expect(breedList).toHaveLength(0);
   expect(status).toBe("loaded");
+});
+
+test("returns an array of breeds when animal is passed", async () => {
+  const breeds = ["a", "b", "c"];
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </Provider>
+    ),
+  });
+
+  await waitFor(() => expect(result.current[1]).toBe("loaded"));
+
+  const [breedList] = result.current;
+  expect(breedList).toEqual(breeds);
 });
