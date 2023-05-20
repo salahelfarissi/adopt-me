cb13be22e3f801dfd5b6acc3718f8a779e14646c
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-05-20 15:13:41 +0200

    Doing tests with renderHook

--- src/__tests__/useBreedList.test.jsx
+++ src/__tests__/useBreedList.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
 import store from "../store";
@@ -15,27 +15,18 @@ const queryClient = new QueryClient({
   },
 });
 
-const getBreedList = (animal) => {
-  let list;
-
-  const TestComponent = () => {
-    list = useBreedList(animal);
-    return null;
-  };
-
-  render(
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <TestComponent />
-      </QueryClientProvider>
-    </Provider>
-  );
-
-  return list;
-};
-
 test("returns an empty array when no animal is passed", async () => {
-  const [breedList, status] = getBreedList();
+  const { result } = renderHook(() => useBreedList(""), {
+    wrapper: ({ children }) => (
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </Provider>
+    ),
+  });
+
+  const [breedList, status] = result.current;
   expect(breedList).toHaveLength(0);
   expect(status).toBe("loaded");
 });
